9f2a5e74aed24edec37cfe65702372d30217ce77
Eduardo Posada da Silva <eduposada@gmail.com>
2026-03-28 18:13:23 -0300

    Ajuste de compatibilidade iPad Mini 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015', // Linguagem mais antiga compatível com iPad Mini 2
-    cssTarget: 'chrome61'
+    // Define o alvo para navegadores de 2015 (compatível com iPad Mini 2 / iOS 12)
+    target: 'es2015', 
+    cssTarget: 'chrome61', // Garante que o visual (CSS) também não quebre
+    minify: 'terser', // Usa um tradutor mais robusto para códigos antigos
   }
 })
